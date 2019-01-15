01862efaeba8da0aad88d1cd7c16e09aee062cf5
adrian <adrianfj@gmail.com>
2019-01-15 16:01:34 -0500

    created sing-in and out functions and tests

--- django-taxi/example_taxi/example_taxi/urls.py
+++ django-taxi/example_taxi/example_taxi/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from example.apis import SignUpView
+from example.apis import SignUpView, LogInView, LogOutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('api/log_in/', LogInView.as_view(), name='log_in'), 
+    path('api/log_out/', LogOutView.as_view(), name='log_out'),
+
 ]
